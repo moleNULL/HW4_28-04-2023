5b997beb26c7ef416e8d88190745949190b91444
moleNULL <94998782+moleNULL@users.noreply.github.com>
2023-05-01 18:48:59 +0300

    Add project files.

---
+++ HW4_28-04-2023/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HW4_28_04_2023.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
